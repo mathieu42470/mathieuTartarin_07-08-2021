c36937b40778c0e005831c1ab2c9fb4f5e11ecc4
tartarin <m.tartarin@hotmail.fr>
2021-08-12 16:50:54 +0200

    changementjs

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 main ()
 
 async function main(){
-               let articles = await getArticles()
-               for (article of articles){
-                              displayArticle(article);
-               }
+  let articles = await getArticles()
+  for (article of articles){
+                 displayArticle(article)
+  }
 }
 
+            
+
 function getArticles() {
-       return fetch("http://localhost:3000/api/teddies")
+  return fetch("http://localhost:3000/api/teddies")
                .then(function(httpBodyResponse) {
                return httpBodyResponse.json()
                })
@@ -18,18 +20,18 @@ function getArticles() {
                .catch(function(error) {
                               alert(Error)
                })
+              }
 
-}
+
 
  function displayArticle(article){
-               let elt = document.getElementById("produitlist");
-              elt.innerHTML += 
-                ` <a href="../article.html" class="card list-none" style="width: 18rem;">
-                <p class="card-img-top"> <img src="${article.imageUrl}" alt="image d'un nounours"></p>
-               <div class="card-body">
-                 <h1 class="list-none"><strong>${article.name}</strong></h1>
-                 <p class="card-text list-none">${article.description}</p>
-                 <p class="list-none"><strong>${article.price}€</strong></p>
-               </div>
-             </div>
-             </a>` }
+  document.getElementById("produitlist").innerHTML +=`
+   <div class="card list-none ms" style="width: 100%;">
+    <img src="http://localhost:3000/images/teddy_1.jpg">
+   <div class="card-body">
+     <h1 class="list-none"><strong>${article.name}</strong></h1>
+     <p class="card-text list-none">${article.description}</p>
+     <p class="list-none"><strong>${article.price}€</strong></p>
+   </div>
+ </div>
+ </div>`}
