4ca5c8d88f31d1e67d29ddcf317a2a5e8ddd30b9
tartarin <m.tartarin@hotmail.fr>
2021-08-30 18:14:56 +0200

    changementpanier

--- js/panier.js
+++ js/panier.js
@@ -1,24 +1,25 @@
 // récuperation du local storage//
    let articlesEnregistres = JSON.parse(localStorage.getItem("article"));
-   // let color = JSON.parse(localStorage.getItem("article"));
+
  
 
 // mise en place panier//
-let paniercommande =document.getElementById("articlecommande");
+    let listeArticle = document.getElementById("listearticle");
 
-if(articlesEnregistres == null){
-}else{
 
- 
+    if(articlesEnregistres === null){
+       let panierVide = `
+       <p> le panier est vide</p>`;
+       listeArticle.innerHTML = panierVide;
+    }else{
           let panierProduit = [];
-          for (k = 0; k <articlesEnregistres.length; k++){
-           let listeArticle = document.getElementById("listearticle");
-          listeArticle.innerHTML +=`
-   <tr class="flex justify-content">
-             <th class="flex"> ${articlesEnregistres[k].article.name}</th >
-             <th  class="flex"> ${articlesEnregistres[k].color}</th >
+          for (a = 0; a < articlesEnregistres.length; a++){
+           panierProduit = panierProduit +`
+          <tr class="flex justify-content">
+             <th class="flex"> ${articlesEnregistres[a].article.name}</th >
+             <th  class="flex"> ${articlesEnregistres[a].color}</th >
              <th>
-                <select name="number" value="nombredepeluche" "class="flex">
+                <select id="valueNounours" name="number" value="nombredepeluche" "class="flex">
                    <option>nombre de peluche</option>
                    <option value="1">1</option>
                    <option value="2">2</option>
@@ -28,34 +29,43 @@ if(articlesEnregistres == null){
                    <option value="6">6</option>
                 </select>
              </th>
-             <th class="flex" value="">${articlesEnregistres[k].article.price/100} €</th > 
-             <th class="flex"> €</th >
-            </tr>
-           `;
+             <th id="prixarticle" class="flex" value="">${articlesEnregistres[a].article.price/100} €</th > 
+             <th id="prixArticleTotal" class="flex"> €</th >
+          </tr>`;    
+          }
+             if(a == articlesEnregistres.length){
+                listeArticle.innerHTML = panierProduit;
+             }
     }
-}
-            
 
 
-
-
-// calcul prix total //
-
-function prixArticleTotal(){
-let prixArticleTotal = document.getElementById("prixtotal");
+// prix total panier //      
+let prixTotalPanier = [];
 for (l = 0; l <articlesEnregistres.length; l++){
-   let prixArticleTotal = articlesEnregistres[l].article.price;
+   let prixArticleTotal = articlesEnregistres[l].article.price/100;
    prixTotalPanier.push(prixArticleTotal);
+   
 }
 const reducer = (accumulator, currentvalue) => accumulator + currentvalue;
 const prixtotal = prixTotalPanier.reduce(reducer,0);
-}
+let totalPanier = document.getElementById("totalprix")
+totalPanier.innerHTML = `
+<p>le prix total est de <strong>${prixtotal}€<strong></p>`;
 
-
-//activation du bouton de commande//
+// bouton commande //
 function boutonCommande(){
-   let  = document.getElementById(boutonCommande);
-   if(boutonCommande){
+   
+if( (document.form1.prenom.value != "") && (document.form1.nom.value != "") && (document.form1.mail.value != "") && (document.form1.adresse.value != "") && (document.form1.ville.value != "") ) {
+   return true;
+   }
+   else{
+      alert("merci d'afficher tous les champs obligatoire");
+      
+      return false;
       
    }
 }
+
+
+
+
